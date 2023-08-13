a731d3397880aaa722f4b66331e532e4710b00b6
iqbal ardiansyah <dibul250899@gmail.com>
2023-08-13 22:29:14 +0700

    08=membaca-data-json

--- src/App.js
+++ src/App.js
@@ -8,12 +8,28 @@ import AsideMenu from "./component/AsideMenu";
 import Footer from "./component/Footer";
 
 function App() {
+  const [items, setItems] = React.useState([]);
+
+  React.useEffect(function () {
+    (async function() {
+      const response = await fetch("https://bwacharity.fly.dev/items", {
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json",
+        },
+      });
+      const { nodes } = await response.json();
+      // const { nodes } = await JSON.parse(response).data;
+      setItems(nodes);
+    })();
+  }, []);
+
   return (
     <>
       <Header />
       <Hero />
       <Browse />
-      <Arrived />
+      <Arrived items={items} />
       <Clients />
       <AsideMenu />
       <Footer />
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ module.exports = {
   theme: {
     extend: {},
     fontFamily: {
-      sans: ['"Josefin Sans"'],
+      sans: ['"Open Sans"'],
       serif: [
         "ui-serif",
         "Georgia",

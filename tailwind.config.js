7d418200d35197ac9ac87f3da416d869c4ed7503
iqbal ardiansyah <dibul250899@gmail.com>
2023-08-13 21:28:40 +0700

    07-cache-cdn

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,29 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './src/**/*.{js,jsx}'
-  ],
+  content: ["./src/**/*.{js,jsx}"],
   theme: {
     extend: {},
+    fontFamily: {
+      sans: ['"Josefin Sans"'],
+      serif: [
+        "ui-serif",
+        "Georgia",
+        "Cambria",
+        '"Times New Roman"',
+        "Times",
+        "serif",
+      ],
+      mono: [
+        "ui-monospace",
+        "SFMono-Regular",
+        "Menlo",
+        "Monaco",
+        "Consolas",
+        '"Liberation Mono"',
+        '"Courier New"',
+        "monospace",
+      ],
+    },
   },
   plugins: [],
-}
-
+};
